798272a7c1785af1d0ad09538a57cbf2bc9a4db0
Miguel Vispo Álvarez <vispoalvarez@gmail.com>
2023-09-02 21:02:15 +0200

    calculadora

---
+++ index.js
@@ -0,0 +1,19 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById("valor-actual");
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAl('.operador');
+
+const display = new Display (displayValorAnterior, displayValorActual)
+
+botonesNumeros.forEach(boton => {
+  boton.addEventListener('click', () =>{
+    display.agregarNumero(boton.innerHTML)
+  })
+})
+
+botonesOperadores.forEach(boton => {
+  boton.addEventListener('click', () => {
+    display.computar(valor)
+  }
+  )})
+
